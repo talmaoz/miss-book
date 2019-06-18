f39793976c698a90bd44c7f743659d905a4fafbd
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-18 15:33:16 +0300

    WIP

--- js/cmps/books/book-list.cmp.js
+++ js/cmps/books/book-list.cmp.js
@@ -4,8 +4,9 @@ export default {
     template: `
     <section>
             <h1>Books</h1>
-            <ul class="car-list">
-                <car-preview v-for="currentCar in books" v-bind:car="currentCar">
+            <ul class="book-list">
+                <car-preview
+                    v-for="currentBook in books" v-bind:car="currentBook">
                 </car-preview>
             </ul>
         </section>
