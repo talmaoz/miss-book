90530e8867bdaea2907c2aefd68116148c41b9d7
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-18 18:03:33 +0300

    WIP

--- js/cmps/books/book-details.cmp.js
+++ js/cmps/books/book-details.cmp.js
@@ -2,14 +2,20 @@ Vue.component('book-details', {
     props: ['book'],
     template: `
         <div>
-            Welcome to boo-details
+            <h2>Book Details:</h2>
             <h3>{{book.title}}</h3>
             <h4>{{book.price}}$</h4>
+            <h4>Published: {{book.publishedDate}}</h4>
+            <h4>#Pages: {{book.pages}}</h4>
+            <h4 v-if:"book.isOnSale">{{saleTxt}}</h4>
             <img v-bind:title="book.title" v-bind:src="book.thumbnail" />
         </div>
     `,
-    created() {
-        console.log("this.book = ", this.book)
+    computed: {
+        saleTxt() {
+            if (this.book.isOnSale) return "On Sale!!"
+            return ''
+        },
     },
 })
 
