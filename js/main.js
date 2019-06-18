d557e97358da443820b374a79807ffe3a2e039c8
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-18 15:18:06 +0300

    Cleaned main.js a bit further

--- js/main.js
+++ js/main.js
@@ -5,19 +5,6 @@ import carApp from './cmps/books/book-app.cmp.js';
 
 var app = new Vue({
     el: '#app',
-    mounted() {
-        // mounted
-    },
-    data: {
-        // data
-    },
-   
-    computed: {
-        // computed
-    },
-    methods: {
-        // methods
-    },
     components: {
         carApp,
     }
