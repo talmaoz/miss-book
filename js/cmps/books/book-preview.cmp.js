28663b9cdfe8f2122b17eeceb9346afc721f3148
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-18 16:50:54 +0300

    Design changes

--- js/cmps/books/book-preview.cmp.js
+++ js/cmps/books/book-preview.cmp.js
@@ -2,8 +2,8 @@ Vue.component('book-preview', {
     props: ['book'],
     template: `
         <li>
-            <h6>{{book.title}}</h6>
-            <h6>{{book.price}}$</h6>
+            <h3>{{book.title}}</h3>
+            <h4>{{book.price}}$</h4>
             <img v-bind:title="book.title" v-bind:src="book.thumbnail" />
         </li>
     `,
